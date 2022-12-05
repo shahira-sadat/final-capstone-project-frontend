355ef8778ddd216ddde0c6301ba19e77bc66f218
wdavidcch <david.13.2121@gmail.com>
2022-12-05 10:11:07 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -17,13 +17,11 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Route>
-
-        <Route path="/navbar" element={<Navbar />}>
-          <Route path="/cars" element={<Cars />} />
-          <Route path="/cars/create" element={<CarCreate />} />
-          <Route path="/details" element={<Details />} />
-          <Route path="/teams" element={<Teams />} />
-        </Route>
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/cars/create" element={<CarCreate />} />
+        <Route path="/navbar" element={<Navbar />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="/teams" element={<Teams />} />
       </Routes>
     </Router>
   );
